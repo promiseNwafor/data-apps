93306d4a4270effd19da932825f146e91a4f3302
promiseNwafor <promisenwafor955@gmail.com>
2025-07-03 15:01:04 +0100

    Remove unused App.test.tsx and ErrorBoundary.tsx files, and update import paths in main.tsx for improved structure.

--- react-hello-world/src/main.tsx
+++ react-hello-world/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import ErrorBoundary from './ErrorBoundary.tsx'
+import App from './App'
+import ErrorBoundary from './components/ErrorBoundary'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
