00abef877bf6cbb43e9ec0f0a4128b69ad8c7385
promiseNwafor <promisenwafor955@gmail.com>
2025-07-14 00:29:28 +0100

    app five

--- app-five/src/lib/prisma.ts
+++ app-five/src/lib/prisma.ts
@@ -1,14 +1,9 @@
-import { PrismaClient } from '../generated/prisma';
+import { PrismaClient } from '@prisma/client'
 
-// Prevent multiple instances of Prisma Client in development
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+  prisma: PrismaClient | undefined
+}
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
-  });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
